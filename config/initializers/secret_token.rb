844a5467dd1b031fb9000b3d5c5c3a699c1fcfab
Nathan Smith <nathasm@gmail.com>
2011-10-10 06:45:11 -0400

    New PES web application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pes::Application.config.secret_token = '71f791697e5c71deda9c99f9b3dc7e9157eebd2fb34b3cfabd081aed7629f8927fefdd2d2ab1bd90f094640500789e9c397e3d8fbb6e499828679fb01faf029f'
